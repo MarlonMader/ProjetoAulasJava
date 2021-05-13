1718468a4e6e50e005072b8aea7d081d17a1288f
MarlonMader <mader9003@gmail.com>
2021-05-13 16:12:28 -0300

    Refatoração Do while

--- src/br/com/senai/HelloWorld.java
+++ src/br/com/senai/HelloWorld.java
@@ -2,6 +2,7 @@ package br.com.senai;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
+		System.out.println("Hello World!!!");
+		System.out.println("AI PSIn Malwee");
 	}
 }
