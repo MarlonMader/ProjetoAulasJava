f013539b9ad0dcbbc0b4df316b19dd021b758a1f
MarlonMader <mader9003@gmail.com>
2021-05-20 16:37:26 -0300

    Aula2 20/05

---
+++ src/br/com/senai/Hojetmj.java
@@ -0,0 +1,12 @@
+package br.com.senai;
+
+
+public class Hojetmj {
+	public static void main(String[] args) {
+	
+		int anoAtual = Integer.parseInt("Em que ano estamos?");
+		int anoNascimento = Integer.parseInt("Em que ano você nasceu?");
+		int idade = anoAtual-anoNascimento;
+		System.out.println(null + "Voce tem:" + idade +  "anos");
+	}
+}
