a973db33e12bcd125881bec21513f2d173edffc2
MarlonMader <mader9003@gmail.com>
2021-05-20 16:37:46 -0300

    Refaturação de Codigo

--- src/br/com/senai/SequenciaComParImpar.java
+++ src/br/com/senai/SequenciaComParImpar.java
@@ -6,8 +6,7 @@ public class SequenciaComParImpar {
 	public static void main(String[] args) {
 		Scanner tec = new Scanner(System.in);
 		
-		int par = 0;
-		int impar = 0;
+
 		
 		System.out.print("Informe o primeiro valor: ");
 		int primeiroValor = tec.nextInt();
@@ -18,25 +17,14 @@ public class SequenciaComParImpar {
 		if (primeiroValor < segundoValor) {
 			while (primeiroValor <= segundoValor) {
 				System.out.println(primeiroValor);
-				if(primeiroValor % 2 == 0) {
-					par++;
-				} else {
-					impar++;
-				}
 				primeiroValor++;
 			}
 		} else {
 			while (primeiroValor > segundoValor) {
 				System.out.println(primeiroValor);
-				if(primeiroValor % 2 == 0) {
-					par++;
-				} else {
-					impar++;
-				}
 				primeiroValor--;
 			}
 		}
-		System.out.println("Total de valores PARES: " + par);
-		System.out.println("Total de valores IMPARES: " + impar);
+
 	}
 }
